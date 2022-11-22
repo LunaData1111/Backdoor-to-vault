83589a9da272bcc928e1809e2b0824b00ed2d83d
Christian Luna <christianlunaduenas@gmail.com>
2022-11-22 12:48:28 -0800

    still working on tag routes.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,10 +3,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  
+  try {
+    const userData = await Tag.findAll({ include: [{model: Product, through: ProductTag}] }); 
+    if (!userData) {
+      res.status(404).json({ message: 'No pr with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
